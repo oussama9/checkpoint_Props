fbc29016f2a693271bfc0a6da5f3704ca86a3bfa
Oussama <guezguezoussama@gmail.com>
2022-05-30 00:07:09 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Profile from "./profile/Profile";
+import userImg from "./images/user.jpg";
+import userImg2 from "./images/user2.jpg";
+import "./App.css";
 
-function App() {
+export default function App() {
+  function handleName(name) {
+    alert(`Hello my name is ${name}`);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile
+        fullName="John Doe"
+        bio="Hello , my name is John Doe and i like web development"
+        profession="Full-Stack js Developer"
+        handleName={handleName}
+      >
+        <img
+          style={{ borderRadius: "100%", width: "200px", height: "200px" }}
+          src={userImg}
+          alt="avatar"
+        />
+      </Profile>
+      <Profile
+        fullName="Jane Doe"
+        bio="Hello , my name is Jane Doe and i like music , i write songs , teach and play the piano"
+        profession="Music Teacher"
+        handleName={handleName}
+      >
+        <img
+          style={{ borderRadius: "100%", width: "200px", height: "200px" }}
+          src={userImg2}
+          alt="avatar"
+        />
+      </Profile>
     </div>
   );
 }
-
-export default App;
